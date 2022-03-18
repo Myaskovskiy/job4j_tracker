536d3869d7e18ac9af72d7d77fde8f2302f3dc8e
vmyaskovskiy <slavav1977@yandex.ru>
2022-03-18 11:25:51 +0300

    3. Liquibase. Интеграционные тесты. [#95881 #8823]new11

--- src/test/java/ru/job4j/tracker/liquibase/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/liquibase/SqlTrackerTest.java
@@ -12,6 +12,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
 import static org.hamcrest.Matchers.is;
@@ -22,7 +23,7 @@ import static org.junit.Assert.assertThat;
  */
 public class SqlTrackerTest {
 
-    static Connection connection;
+    public static Connection connection;
 
     @BeforeClass
     public static void initConnection() {
@@ -56,25 +57,23 @@ public class SqlTrackerTest {
     @Test
     public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        tracker.add(item);
+        Item item =  tracker.add(new Item("item"));
         assertThat(tracker.findById(item.getId()), is(item));
     }
 
     @Test
     public void whenSaveItemAndFindByNameThenMustBeTheSame() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        tracker.add(item);
-        assertThat(tracker.findByName("item").get(0), is(item));
+        Item item =  tracker.add(new Item("item"));
+        Item item1 = tracker.add(new Item("item"));
+        assertThat(tracker.findByName("item"), is(List.of(item, item1)));
     }
 
     @Test
     public void whenReplaceItemAndFindByNameThenMustBeTheSame() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
+        Item item =  tracker.add(new Item("item"));
         Item item1 = new Item("test");
-        tracker.add(item);
         tracker.replace(item.getId(), item1);
         assertThat(tracker.findByName("test").get(0).getName(), is("test"));
     }
@@ -82,22 +81,20 @@ public class SqlTrackerTest {
     @Test
     public void whenFindAllItemAndFindSizeThenMustBeTheSame() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        Item item1 = new Item("item");
-        tracker.add(item);
-        tracker.add(item1);
+        Item item =  tracker.add(new Item("item"));
+        Item item1 = tracker.add(new Item("item"));
         assertThat(tracker.findAll().size(), is(2));
+        assertThat(tracker.findAll(), is(List.of(item, item1)));
     }
 
     @Test
     public void whenDellItemAndFindSizeThenMustBeOne() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        Item item1 = new Item("test");
-        tracker.add(item);
-        tracker.add(item1);
+        Item item =  tracker.add(new Item("item"));
+        Item item1 = tracker.add(new Item("test"));
         assertThat(tracker.delete(item1.getId()), is(true));
         assertThat(tracker.findAll().size(), is(1));
         assertThat(tracker.findByName("item").get(0).getName(), is("item"));
+        assertThat(tracker.findAll(), is(List.of(item)));
     }
 }
